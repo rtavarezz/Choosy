b8ddee155009b7766a4816b9344bb045557a53d2
rikoeldon <106416799+rikoeldon@users.noreply.github.com>
2025-07-24 02:35:12 -0400

    vercel fix

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,21 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
-import { AuthProvider } from '../lib/auth';
 import { DarkModeProvider } from '../lib/darkMode';
 
-// Dynamically import Analytics to avoid SSR issues
-const Analytics = dynamic(() => import('@vercel/analytics/react').then(mod => ({ default: mod.Analytics })), {
-  ssr: false,
-});
-
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DarkModeProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-        <Analytics />
-      </AuthProvider>
+      <Component {...pageProps} />
     </DarkModeProvider>
-  );
-} 
+  )
+}
+
+export default MyApp 
